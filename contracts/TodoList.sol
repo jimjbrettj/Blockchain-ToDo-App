9382079eef532667e0cd1797731a2dc0c00a097c
jimjbrettj <jimjbrettj@yahoo.com>
2019-08-19 13:02:18 -0700

    some changes in smart contract and dependencies

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint taskcount = 0;
+
+	constructor() public {
+		createTask("This is the first tast I'm making");
+	}
+	
+  uint taskCount = 0;
+
+  struct Task {
+	  uint id;
+	  string content;
+	  bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+  	taskCount ++;
+  	tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
